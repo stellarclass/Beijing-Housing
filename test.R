122e43d4782cee5a7f1f4750ec4e398a87f73851
Iman <iman.lau@gmail.com>
2018-11-11 22:32:35 -0500

    mice imputation, includes DOM

--- test.R
+++ test.R
@@ -5,10 +5,12 @@ library(ggplot2)
 library(ggmap)
 library(Hmisc)
 library(mice)
+library(miceadds)
+library(corrplot)
+library(lubridate)
 
 # library(tidyr)
 # library(cluster)
-# library(lubridate)
 # library(rgl)
 # library(maptools)
 # library(ggpubr)
@@ -24,7 +26,6 @@ library(mice)
 # library(ROCR)
 # library(pROC)
 # library(rlist)
-
 # library(corrplot)
 # library(ggcorrplot)
 # library(DMwR)
@@ -39,53 +40,12 @@ library(mice)
 data_dir = "data"
 
 ### Loading data
-data=read.csv(paste(data_dir,"original_data.csv",sep="/"), header = TRUE, sep = ",",na.strings = c("nan","#NAME?"))
+data=read.csv(paste(data_dir,"cleaned_data.csv",sep="/"), header = TRUE, sep = ",",na.strings = c("NA", "nan","#NAME?"))
 
 bjmap <- get_map(location =c(left=115.9133, bottom=39.6966, right=116.8252, top=40.1831))
 beijingMap <- ggmap(bjmap)
 
 ### Data Preprocessing
-
-#factorize everything
-for(col in c("buildingType","kitchen","district","bathRoom", "livingRoom", "drawingRoom", "constructionTime", "renovationCondition", "elevator", "fiveYearsProperty", "subway", "district")) {
-  data[,col] = as.factor(data[,col])
-}
-
-#first, let's take a look at NA
-
-colSums(is.na(data))
-
-#looks like there's a consistent number of NA values for some columns so let's take a closer look
-NAdata <- which(is.na(data$subway))
-data[NAdata, ]
-
-#looks like some values were swapped! let's fix this up
-#thanks to having the URLs, we can see how things were mixed up
-#looks like there is no room data input for the NA rows 
-
-data$subway[NAdata] <- data$buildingStructure[NAdata]
-
-#convert to numbers - reinforced = 6, hybrid = 1
-
-BS6 <- which(data$floor == "Reinforced Concrete Structure")
-BS1 <- which(data$floor == "Hybrid Structure")
-data$buildingStructure[BS6] <- 6
-data$buildingStructure[BS1] <- 1
-
-data$floor[NAdata] <- data$drawingRoom[NAdata]
-data$drawingRoom[NAdata] <- NA
-
-data$elevator[NAdata] <- data$constructionTime[NAdata]
-data$constructionTime[NAdata] <- data$bathRoom[NAdata]
-data$bathRoom[NAdata] <- NA
-
-data$fiveYearsProperty[NAdata] <- data$renovationCondition[NAdata]
-data$renovationCondition[NAdata] <- data$ladderRatio[NAdata]
-data$ladderRatio[NAdata] <- data$buildingType[NAdata]
-data$buildingType[NAdata] <- data$kitchen[NAdata]
-data$kitchen[NAdata] <- NA
-
-data$livingRoom[NAdata] <- NA
 
 #reno condition can't be 0 - change to NA 
 data$renovationCondition[data$renovationCondition == 0] <- NA
@@ -94,8 +54,21 @@ data$renovationCondition[data$renovationCondition == 0] <- NA
 temp <- as.data.frame(str_split_fixed(data$floor, " ", 2))
 colnames(temp) <- c("floorType", "floorLevel")
 data <- cbind(data, temp)
+data$floorLevel=as.integer(data$floorLevel)
 
-#factor elevator, fiveyearsproperty, subway and change to yes/no
+#remove unused columnns
+data <- data[, !colnames(data) %in% c("url","id", "floor", "DOM")]
+
+#type conversion
+data$tradeTime = ymd(data$tradeTime)
+data$drawingRoom = as.integer(data$drawingRoom)
+data$bathRoom = as.integer(data$bathRoom)
+data$district=as.factor(data$district)
+data$constructionTime = as.integer(data$constructionTime)
+
+data$buildingType = factor(mapvalues(data$buildingType, from = c(1, 2, 3, 4), to = c("tower", "bungalow", "plate-tower combination", "plate")))
+data$renovationCondition = factor(mapvalues(data$renovationCondition, from = c(1, 2, 3, 4), to = c("other", "rough", "simplicity", "hardcover")))
+data$buildingStructure = factor(mapvalues(data$buildingStructure, from = c(1, 2, 3, 4, 5, 6), to = c("unknown", "mixed", "brick and wood", "brick and concrete", "steel", "steel-concrete composite")))
 data$elevator = factor(mapvalues(data$elevator, from = c(0, 1), to = c("no", "yes")))
 data$fiveYearsProperty = factor(mapvalues(data$fiveYearsProperty, from = c(0, 1), to = c("no", "yes")))
 data$subway = factor(mapvalues(data$subway, from = c(0, 1), to = c("no", "yes")))
@@ -108,25 +81,60 @@ boxplot(data$ladderRatio, las = 1, ylim=c(0,2))
 
 data$ladderRatio[data$ladderRatio > 1] <- NA
 
-#remove unused columnns
-data <- data[, !colnames(data) %in% c("url","id", "floor")]
-
 #what to do with NAs?
 
 colSums(is.na(data))
 
-#ladder, rooms use mean/median?
-data$ladderRatio=impute(data$ladderRatio, mean)
-data$livingRoom=impute(data$livingRoom, mean)
-data$drawingRoom=impute(data$drawingRoom, mean)
-data$kitchen=impute(data$kitchen, mean)
-data$bathRoom=impute(data$bathRoom, mean)
+#use mice imputation
+numeric_cols = sapply(data, is.numeric)
+data_num_only=data[,numeric_cols]
+cor(data_num_only)
+corrplot(cor(data_num_only), method = "circle")
 
-#buildingtype, community, DOM use mice imputation
 set.seed(12345)
 
 # Perform mice imputation, excluding certain less-than-useful variables:
-mice_mod <- mice(data[, !names(data) %in% 'totalPrice'], method='rf') 
+mice_mod <- mice(data[, !names(data) %in% c('Lng', 'Lat', 'Cid', 'followers', 'totalPrice', 'price')], pred = quickpred(data[, !names(data) %in% c('Lng', 'Lat', 'Cid', 'followers', 'totalPrice', 'price')], mincor=0.1, minpuc=0.3)) 
 
 # Save the complete output 
 mice_output <- complete(mice_mod)
+
+# Plot distributions
+par(mfrow=c(1,2))
+hist(data$DOM, freq=F, main='Original Data', 
+     col='darkgreen', ylim=c(0,0.04))
+hist(mice_output$DOM, freq=F, main='MICE Output', 
+     col='lightgreen', ylim=c(0,0.04))
+
+hist(as.numeric(data$buildingType), freq=F, main='Original Data', 
+     col='darkgreen', ylim=c(0,0.04))
+hist(as.numeric(mice_output$buildingType), freq=F, main='MICE Output', 
+     col='lightgreen', ylim=c(0,0.04))
+
+hist(data$ladderRatio, freq=F, main='Original Data', 
+     col='darkgreen', ylim=c(0,0.04))
+hist(mice_output$ladderRatio, freq=F, main='MICE Output', 
+     col='lightgreen', ylim=c(0,0.04))
+
+#doesn't work, don't run
+hist(as.numeric(data$communityAverage), freq=F, main='Original Data', 
+     col='darkgreen', ylim=c(0,0.04))
+hist(as.numeric(mice_output$communityAverage), freq=F, main='MICE Output', 
+     col='lightgreen', ylim=c(0,0.04))
+
+hist(data$constructionTime, freq=F, main='Original Data', 
+     col='darkgreen', ylim=c(0,0.04))
+hist(mice_output$constructionTime, freq=F, main='MICE Output', 
+     col='lightgreen', ylim=c(0,0.04))
+
+#replace with imputed values
+
+data$DOM <- mice_output$DOM
+data$buildingType <- mice_output$buildingType
+data$ladderRatio <- mice_output$ladderRatio
+data$communityAverage <- mice_output$communityAverage
+data$constructionTime <- mice_output$constructionTime
+
+#save the imputation
+write.csv(as.data.frame(mice_output), file="data/mice_output.csv")
+write.csv(as.data.frame(data), file="data/imputed_data.csv")
